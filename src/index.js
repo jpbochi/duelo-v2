1a26e852d2ef945678740193375633c5abb78745
JP Bochi <joao.bochi@rackspace.com>
2016-11-04 12:21:18 +0100

    mozilla/neo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './app';
+import routes from './routes';
+import Layout from './layout';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+export const reducers = {
+  title: state => state
+};
+
+export const initialState = {
+  title: 'duelo-v2'
+};
+
+App({ reducers, initialState, Layout, routes }).render();
